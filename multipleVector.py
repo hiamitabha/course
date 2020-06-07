3409dd188b38f97d5b1a8ec55200bdc108841d03
Amitabha Banerjee <hiamitabha@gmail.com>
2020-06-07 01:22:30 -0700

    Final changes to program that allows all three connection mode to a set of Vectors: syncronous, asyncronous, and multiprocessor
    Now supporting multiple Vectors (more than two)

--- multipleVector.py
+++ multipleVector.py
@@ -5,8 +5,8 @@ import argparse
 from multiprocessing import Pool
 
 #Animations
-_ANIM1 = 'anim_fistbump_requesttwice_01'
-_ANIM2 = 'anim_fistbump_requestoncelong_01'
+_ANIMATION1 = 'anim_fistbump_requesttwice_01'
+_ANIMATION2 = 'anim_fistbump_requestoncelong_01'
 
 def connectAsync(serial):
    # Create a Robot object
@@ -36,45 +36,53 @@ def playAnimationsAsync(serial, animation):
    disconnect(robot1)
    disconnect(robot2)
 
-def playAnimationOnRobotSync(serial, animation):
+def playAnimationOnRobotSync(input):
+   (serial, animation) = input
    robot = connectSync(serial)
    robot.anim.play_animation(animation)
    disconnect(robot)
-   
 
 def playAnimationsMultiProcess(serial, animation):
    inputs = zip(serial, animation)
-   print (inputs)
    with Pool(2) as pool:
       pool.map(playAnimationOnRobotSync, inputs)
 
 def playAnimationsSync(serial, animation):
    inputs = zip(serial, animation)
-   print (inputs)
    for (singleSerial, singleAnimation) in inputs:
-      playAnimationOnRobotSync(singleSerial, singleAnimation)
+      playAnimationOnRobotSync((singleSerial, singleAnimation))
 
 def parse():
    parser = argparse.ArgumentParser()
-   parser.add_argument("-s1", "--serial1",
-                       help="Serial Number of the first robot",
-                       default=_VECTOR1_SERIAL)
-   parser.add_argument("-s2", "--serial2",
-                       help="Serial Number of the second robot",
-                       default=_VECTOR2_SERIAL)
-   parser.add_argument("-a1", "--animation1",
-                       help="Animation required of the first robot",
-                       default=_ANIM1)
-   parser.add_argument("-a2", "--animation2",
-                       help="Animation required of the second robot",
-                       default=_ANIM2)
+   parser.add_argument("-s", "--serial",
+                       help="Serial Numbers")
+   parser.add_argument("-a", "--animation",
+                       help="Animations. One per serial number",
+                       default=_ANIMATION1 + ',' + _ANIMATION2)
+   parser.add_argument("-m", "--mode",
+                       help="Choose whether to connect to Vector"
+                       " in the following modes: sync, async, multiprocess",
+                       default="sync")
    args = parser.parse_args()
    return args
 
 def main():
    args = parse()
-   #playAnimationsAsync([args.serial1, args.serial2],[args.animation1, args.animation2])
-   playAnimationsSync([args.serial1, args.serial2],[args.animation1, args.animation2])
+   serialNumbers = args.serial.split(',')
+   animations = args.animation.split(',')
+   print (serialNumbers)
+   print (animations)
+   startTime = time.time()
+   if args.mode == "async":
+      playAnimationsAsync(serialNumbers, animations)
+   elif args.mode == "sync":
+      playAnimationsSync(serialNumbers, animations)
+   elif args.mode == "multiprocess":
+      playAnimationsMultiProcess(serialNumbers, animations)
+   duration = time.time() - startTime
+   print (f"Duration is {duration} seconds")
+   time.sleep(1)
 
 if __name__ == "__main__":
-   main() 
+   main()
+
